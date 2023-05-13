b807c984f996d05c5c5eb3dc18478e92ada63c1f
taravann <heng.t.taravann@gmail.com>
2023-05-13 22:56:44 +1000

    feat: add icons to project

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBookBookmark, faUser, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import App from './App.vue'
+
+library.add(faBookBookmark, faUser, faPaperPlane);
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.mount('#app')
