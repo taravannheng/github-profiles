5a5484eb6026ece14f284e1166e26dd582019157
taravann <heng.t.taravann@gmail.com>
2023-05-13 23:39:35 +1000

    feat: add profile component for displaying avatar, profile info and bio

---
+++ src/components/profile/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="profile" v-if="profileData" :style="profileStyle">
+    <Avatar :avatar-data="profileData" style="margin-bottom: 8px;" />
+    <ProfileInfo :profile-info-data="profileData" />
+    <Bio :bio-data="profileData" style="margin-top: 32px;" />
+  </div>
+</template>
+
+<script lang="ts">
+import Avatar from '../avatar/Avatar.vue';
+import ProfileInfo from '../profile-info/ProfileInfo.vue';
+import Bio from '../bio/Bio.vue';
+
+export default {
+  name: 'Profile',
+  props: {
+    profileStyle: {
+      type: String,
+      required: false,
+    },
+    profileData: {
+      type: Object,
+      required: false,
+    }
+  },
+  components: {
+    Avatar,
+    ProfileInfo,
+    Bio,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
