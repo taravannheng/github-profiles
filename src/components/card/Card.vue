207aa1a16f50cdebc09d00e184dca81896f313a2
taravann <heng.t.taravann@gmail.com>
2023-05-13 14:03:19 +1000

    feat: add card and suggestion components

---
+++ src/components/card/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card" :style="`${cardStyle}`"> 
+    <p class="card__text d-flex flex-row align-items-center" :style="`cursor: ${isClickable ? 'pointer;' : 'default;'} ${cardTextStyle}}`">{{ content }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Card',
+  props: {
+    cardStyle: {
+      type: String,
+      required: false,
+    },
+    cardTextStyle: {
+      type: String,
+      required: false,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isClickable: {
+      type: Boolean,
+      required: false,
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: clamp(100%, 100%, 100%);
+  height: clamp(56px, 56px, 56px);
+  margin: 0;
+  border-left-width: 4px;
+  border-left-color: map-get($colors, dark-primary);
+  border-radius: 0;
+  background-color: map-get($colors, medium-primary);
+  color: map-get($colors, lightest);
+  overflow: hidden;
+
+  &__text {
+    width: clamp(100%, 100%, 100%);
+    height: clamp(100%, 100%, 100%);
+    padding-left: 18px;
+    margin: 0;
+    transition: background-color 0.3s linear;
+
+    &:hover {
+      background-color: map-get($colors, dark-primary);
+    }
+  }
+}
+</style>
+
---
+++ src/components/suggestion/Suggestion.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="suggestion" :style="`${suggestionStyle}`">
+    <h1 class="suggestion__title">Users</h1>
+    <small class="suggestion__caption" v-if="suggestionData.length">Here's what we found...</small>
+    <div class="suggestion__card-container d-flex flex-column gap-1">
+      <div class="suggestion__card" v-for="(item, index) in suggestionData" v-if="suggestionData" :key="index">
+        <Card :content="item.login" /> <!-- add is-clickable prop to the card component when able to fill the searchbox with the content of the card -->
+      </div>
+      <div class="suggestion__card-empty" v-if="!suggestionData.length"><span class="card-empty__text"></span>We couldn't find any users...</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import Card from "../card/Card.vue";
+
+export default {
+  name: 'Suggestion',
+  props: {
+    suggestionStyle: {
+      type: String,
+      required: false,
+    },
+    suggestionData: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    Card,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.suggestion {
+  width: clamp(100%, 100%, 100%);
+  height: clamp(100%, 100%, 100%);
+
+  &__title {
+    color: map-get($colors, lightest);
+    font-size: map-get($font-sizes, h4);
+    font-weight: map-get($font-weights, h4);
+  }
+
+  &__caption {
+    color: map-get($colors, light);
+    font-size: map-get($font-sizes, body1);
+    font-weight: map-get($font-weights, body1);
+  }
+
+  &__card-container {
+    margin-top: 24px;
+  }
+
+  &__card-empty {
+    width: clamp(100%, 100%, 100%);
+    height: clamp(100%, 100%, 100%);
+  }
+}
+
+.card-empty {
+
+  &__text {
+    color: map-get($colors, light);
+  }
+}
+</style>
+
