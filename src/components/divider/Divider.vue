1d3e3e6040aa6ec7524285b7c891b7646308b8df
taravann <heng.t.taravann@gmail.com>
2023-05-13 23:31:05 +1000

    feat: add divider component

---
+++ src/components/divider/Divider.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="divider" :style="`${dividerStyle}`">
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Divider',
+  props: {
+    dividerStyle: {
+      type: String,
+      required: false,
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.divider {
+  width: clamp(100%, 100%, 100%);
+  height: 1px;
+  background-color: map-get($colors, dark-primary);
+}
+</style>
+
