7e2247b6afb0781148874770446cd100ca262c64
taravann <heng.t.taravann@gmail.com>
2023-05-13 23:43:01 +1000

    feat: add profile display component for displaying profile and repo

---
+++ src/components/profile-display/ProfileDisplay.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="profile-display" v-if="profileDisplayData" :style="profileDisplayStyle">
+    <div class="profile-display__content">
+      <Profile :profile-data="profileDisplayData" />
+      <Divider style="margin: 40px 0;" />
+      <Repo :repo-data="profileDisplayData" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Profile from '../profile/Profile.vue';
+import Repo from '../repo/Repo.vue';
+import Divider from '../divider/divider.vue';
+
+export default {
+  name: 'ProfileDisplay',
+  props: {
+    profileDisplayStyle: {
+      type: String,
+      required: false,
+    },
+    profileDisplayData: {
+      type: Object,
+      required: false,
+    }
+  },
+  components: {
+    Profile,
+    Repo,
+    Divider,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.profile-display {
+
+  margin-left: 24px;
+  margin-right: 24px;
+
+}
+</style>
+
