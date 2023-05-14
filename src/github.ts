7f93e4b39b83b0bcb2f1f9093eef6613cf9510f3
taravann <heng.t.taravann@gmail.com>
2023-05-14 21:48:28 +1000

    refactor: transform user data before return

--- src/github.ts
+++ src/github.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import _ from "lodash";
 
+import {type UserBasicInfo} from "@/types";
+
 const baseURL = "https://api.github.com";
 
 
@@ -8,8 +10,29 @@ const getUsers = async (username: string) => {
   try {
     if (!_.isEmpty(username)) {
       const response = await axios.get(`${baseURL}/search/users?q=${username}`);
+      let users = response.data.items.map((item: any) => {
+        const user = {
+          login: item.login,
+          name: item.name,
+          email: item.email,
+          avatar_url: item.avatar_url,
+          html_url: item.html_url,
+          followers: item.followers,
+          following: item.following
+        };
+        return user;
+      });
 
-      return response.data;
+      // replace undefined data with N/A or 0
+      const updatedUsers = users.map((user: UserBasicInfo) => ({
+        ...user,
+        followers: user.followers ?? '0',
+        following: user.following ?? '0',
+        name: user.name ?? 'N/A',
+        email: user.email ?? 'N/A'
+      }));
+      
+      return updatedUsers;
     }
   } catch (error) {
     console.error(error);
