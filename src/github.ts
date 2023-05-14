7b433805a17b702b4a7c569ed44950bd563497ca
taravann <heng.t.taravann@gmail.com>
2023-05-15 01:31:41 +1000

    refactor: add new function to simplify GET request when fetching user data for profile display

--- src/github.ts
+++ src/github.ts
@@ -3,6 +3,11 @@ import _ from "lodash";
 
 const baseURL = "https://api.github.com";
 
+import {
+  type UserBasicData,
+  type LatestRepoBasicData,
+  type UserData,
+} from "@/types";
 
 const getUsers = async (username: string) => {
   try {
@@ -15,7 +20,7 @@ const getUsers = async (username: string) => {
         };
         return user;
       });
-      
+
       return users;
     }
   } catch (error) {
@@ -23,11 +28,91 @@ const getUsers = async (username: string) => {
   }
 };
 
-const getUserInfo = async (username: string) => {
+const getUserData = async (username: string) => {
+  try {
+    let userData: UserData = {
+      avatarUrl: "",
+      htmlUrl: "",
+      username: "",
+      followers: "",
+      following: "",
+      name: "",
+      email: "",
+      latestRepoName: null,
+      latestRepoHtmlUrl: "",
+      latestRepoDescription: "",
+      latestRepoLanguages: "",
+      latestCommit: "",
+    };
+    const basicData = await getUserBasicData(username);
+
+    if (!_.isEmpty(basicData)) {
+      userData = {
+        ...userData,
+        avatarUrl: basicData?.avatarUrl ?? "",
+        htmlUrl: basicData?.htmlUrl ?? "",
+        username: basicData?.username ?? "",
+        followers: basicData?.followers ?? "0",
+        following: basicData?.following ?? "0",
+        name: basicData?.name ?? "N/A",
+        email: basicData?.email ?? "N/A",
+      };
+    }
+
+    const repos = await getRepos(username);
+
+    if (!_.isEmpty(repos)) {
+      const latestRepoBasicData = await getLatestRepoBasicData(username);
+      const latestRepoLanguages = await getLatestRepoLanguages(username);
+      const latestCommit = await getLatestCommit(username);
+
+      if (latestRepoBasicData) {
+        userData = {
+          ...userData,
+          latestRepoName: latestRepoBasicData.name,
+          latestRepoHtmlUrl: latestRepoBasicData.htmlUrl,
+          latestRepoDescription: latestRepoBasicData.description,
+        };
+      }
+
+      if (latestRepoLanguages) {
+        userData = {
+          ...userData,
+          latestRepoLanguages: latestRepoLanguages,
+        };
+      }
+
+      if (latestCommit) {
+        userData = {
+          ...userData,
+          latestCommit: latestCommit,
+        };
+      }
+    }
+
+    return userData;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getUserBasicData = async (username: string) => {
   try {
     if (!_.isEmpty(username)) {
       const response = await axios.get(`${baseURL}/users/${username}`);
-      return response.data;
+      const { avatar_url, html_url, login, followers, following, name, email } =
+        response.data;
+      const userBasicData: UserBasicData = {
+        avatarUrl: avatar_url,
+        htmlUrl: html_url,
+        username: login,
+        followers: followers ?? "0",
+        following: following ?? "0",
+        name: name ?? "N/A",
+        email: email ?? "N/A",
+      };
+
+      return userBasicData;
     }
   } catch (error) {
     console.error(error);
@@ -38,81 +123,114 @@ const getRepos = async (username: string) => {
   try {
     if (!_.isEmpty(username)) {
       const response = await axios.get(`${baseURL}/users/${username}/repos`);
-      return response.data;  
+
+      return response.data;
     }
   } catch (error) {
     console.error(error);
   }
 };
 
-const getLatestRepo = async (username: string) => {
+const getLatestRepoName = async (username: string) => {
   const repos = await getRepos(username);
 
   // get the latest repo
-  let mostRecentRepo = repos[0];
+  let latestRepo = repos[0];
   if (repos) {
     for (let i = 1; i < repos.length; i++) {
       const currentRepo = repos[i];
       const currentRepoDate = new Date(currentRepo.updated_at);
-      const mostRecentRepoDate = new Date(mostRecentRepo.updated_at);
+      const latestRepoDate = new Date(latestRepo.updated_at);
 
-      if (currentRepoDate.getTime() > mostRecentRepoDate.getTime()) {
-        mostRecentRepo = currentRepo;
+      if (currentRepoDate.getTime() > latestRepoDate.getTime()) {
+        latestRepo = currentRepo;
       }
     }
   }
 
+  return latestRepo?.name ?? null;
+};
+
+const getLatestRepoBasicData = async (username: string) => {
+  const latestRepoName = await getLatestRepoName(username);
+
   try {
-    if (!_.isEmpty(username) && !_.isEmpty(mostRecentRepo)) {
-      const response = await axios.get(`${baseURL}/repos/${username}/${mostRecentRepo.name}`);
-      return response.data;
+    if (!_.isEmpty(username) && !_.isEmpty(latestRepoName)) {
+      const response = await axios.get(
+        `${baseURL}/repos/${username}/${latestRepoName}`
+      );
+      const { name, html_url, description } = response.data;
+      const latestRepo: LatestRepoBasicData = {
+        name: name ?? null,
+        description: description ?? "N/A",
+        htmlUrl: html_url ?? "N/A",
+      };
+
+      return latestRepo;
     }
+
+    return null;
   } catch (error) {
     console.error(error);
   }
 };
 
-const getRepoLanguages = async (username: string) => {
-  const latestRepo = await getLatestRepo(username);
-  const latestRepoName = latestRepo?.name ?? null;
+const getLatestRepoLanguages = async (username: string) => {
+  const latestRepoName = await getLatestRepoName(username);
 
   try {
+    let languages: string = "N/A";
+
     if (!_.isEmpty(username) && !_.isEmpty(latestRepoName)) {
-      const response = await axios.get(`${baseURL}/repos/${username}/${latestRepoName}/languages`);
-      return response.data;
+      const response = await axios.get(
+        `${baseURL}/repos/${username}/${latestRepoName}/languages`
+      );
+
+      if (response.data) {
+        const languageList = Object.keys(response.data);
+
+        if (languageList.length) {
+          languages = languageList.join(" · ");
+        } else {
+          languages = "N/A";
+        }
+      }
+
+      return languages;
     }
 
-    return null;
-  } catch(error) {
+    return languages;
+  } catch (error) {
     console.error(error);
   }
-}
+};
 
 const getLatestCommit = async (username: string) => {
-  const latestRepo = await getLatestRepo(username);
-  const latestRepoName = latestRepo?.name ?? null;
+  const latestRepoName = await getLatestRepoName(username);
 
   try {
     if (!_.isEmpty(username) && !_.isEmpty(latestRepoName)) {
-      const response = await axios.get(`${baseURL}/repos/${username}/${latestRepoName}/commits?per_page=1`);
+      const response = await axios.get(
+        `${baseURL}/repos/${username}/${latestRepoName}/commits?per_page=1`
+      );
       const commits = response.data;
-    
+
       commits.sort((a: any, b: any) => {
         const dateA = new Date(a.commit.author.date);
         const dateB = new Date(b.commit.author.date);
         return dateB.getTime() - dateA.getTime();
       });
-      
+
       // get the latest commit
-      const latestCommit = commits[0];
-    
+      const latestCommit = commits[0].commit.message ?? "N/A";
+
       return latestCommit;
     }
 
-    return null;
+    return "N/A";
   } catch (error) {
     console.error(error);
   }
 };
 
-export { getUsers, getUserInfo, getLatestRepo, getRepoLanguages, getLatestCommit };
+export { getUsers, getUserData };
